75befb1efa1abf1333c7740e5775c49c0506981c
Pavlo Pravdyvtsev <54671242+pravdyvtsev@users.noreply.github.com>
2023-02-04 22:58:01 +0200

    ok

---
+++ data/slide12.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{F78CCF4B-98FF-4093-AFA6-78618EC6246E}'];loadHandler&&loadHandler(11, '<div id="spr0_76d03f9"><div id="spr1_76d03f9" class="kern slide"><img id="img0_76d03f9" src="data/img0.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_76d03f9" class="kern slide"></div></div>', '{"s":[]}');})();
